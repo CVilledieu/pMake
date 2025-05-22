3495134db9cbc69319190faa3b2a2220e8221a34
Christopher <villedieucr@gmail.com>
2025-05-22 00:23:51 -0700

    parsing set

--- src/main.c
+++ src/main.c
@@ -2,51 +2,32 @@
 #include <stdlib.h>
 
 
-typedef struct { 
+
+typedef struct Options{
     char *fPath;
     char *fName;
-    Options options;
-}Project;
-
-typedef struct {
     int git;
     int make;
     int language; 
 }Options;
 
-int getLanguage(char *lang);
+void parseInput(int argc, char *argv[], Options* options);
 
-const char *default_path = "default/path";
-const char *default_name = "default_name";
+const char *default_path = "./%s\n";
+const char *default_name = "newProject";
 
 
 int main(int argc, char *argv[]) {
-    Project *proj = malloc(sizeof(Project));
-    Options *options = malloc(sizeof(Options));
-    if (argc < 2) {
-        printf("No arguments provided. Using default project name and path.\n");
-        proj->fPath = default_path;
-        proj->fName = default_name;
-    }
-    for (int i = 0; i < argc; i++) {
-        switch (argv[i][0]){
-            case '-':
-                switch (argv[i][1]){
-                    case 'g':
-                        proj->options.git = 1;
-                        break;
-                    case 'm':
-                        proj->options.make = 1;
-                        break;
-                    case 'l':
-                        proj->options.language = getLanguage(argv[i+1]);
-                        break;
-                    default:
-                        printf("Unknown option: %s\n", argv[i]);
-                }
-                break;
-        }
-        
-    }
-    FILE *file = fopen(argv[1], "r");
+    Options* options = malloc(sizeof(Options));
+    parseInput(argc, argv, &options);
+    FILE *fp;
+    char projectName = "test";
+    char fName = "test.c";
+    sprintf(".//%s//",projectName, fName);
+
+    return 0;    
+}
+
+void parseInput(int argc, char *argv[], Options* options) {
+    printf("%s\n", "Parse input");
 }
